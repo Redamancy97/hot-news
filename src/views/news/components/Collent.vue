8d6c76ed3e8eb6c1c35adb52807205ceee76be19
root <1516689161@qq.com>
2020-07-22 10:42:38 +0800

    全部实现

--- src/views/news/components/Collent.vue
+++ src/views/news/components/Collent.vue
@@ -5,7 +5,7 @@
         <!--            <van-icon v-show="!like" name="like-o" @click="enshrine"/>-->
         <!--            <van-icon v-show="like" name="like" color="red" @click="enshrine"/>-->
         <!--        </div>-->
-        <van-icon class="collent" name="like-o"
+        <van-icon class="collect" name="like-o"
                 style="position: fixed; right: 35px;bottom: 55px;"
                 :color="isCollect?'red':'black'"
                 @click="collect"
@@ -15,9 +15,10 @@
     </div>
 </template>
 <script>
-    import {Toast} from "vant";
+    import {Toast,Dialog} from "vant";
     import {collectArticleId,} from "../../../api/hot-news-api";
     import {mapState} from "vuex";
+
 
     export default {
         name: "Collent",
@@ -46,68 +47,72 @@
             //     })
         },
         methods: {
-            // enshrine() {
-            //     if (this.$store.state.isLogin) {
-            //         if (this.like==true) {
-            //             Dialog.confirm({
-            //                 title: '你不喜欢了？',
-            //                 message: '那就取消吧',
-            //             }).then(() => {
-            //                 this.like = false
-            //                 // localStorage.setItem("collect", this.like);
-            //                 })
-            //         } else {
-            //             this.like = true
-            //             Toast.success('收藏成功');
-            //             collectArticleId(this.articleId).then(res => {
-            //                 console.log(res)
-            //             })
-            //         }
-            //     } else {
-            //         // this.$router.push("/my/login");
-            //         Dialog.confirm({
-            //             title: '您不登陆没办法收藏哦',
-            //             message: '单击确认可以去到登录页面哦',
-            //             confirmButtonText:'这就去登录',
-            //             confirmButtonColor:'#424874',
-            //             cancelButtonText:'就不去就不去'
-            //         })
-            //             .then(() => {
-            //                 // on confirm
-            //                 this.$router.push("/my/login");
-            //             })
-            //             .catch(() => {
-            //                 // on cancel
-            //             });
-            //         // Toast.fail('请先登录，再进行操作');
-            //     }
-            // },
             collect() {
-                if (this.isCollect) {
-                    collectArticleId(this.articleId)
-                        .then(res => {
+                if (this.$store.state.isLogin) {
+                    if (this.isCollect) {
+                        collectArticleId(this.articleId)
+                            .then(res => {
+                                console.log(res);
+                                if (res.data.code == 0) {
+                                    Toast.success("取消收藏");
+                                    this.$emit("collect")
+                                }
+                            })
+                    } else {
+                        collectArticleId(this.articleId).then(res => {
                             console.log(res);
                             if (res.data.code == 0) {
-                                Toast.success("取消收藏");
+                                Toast.success("收藏成功");
                                 this.$emit("collect")
                             }
                         })
+                    }
                 } else {
-                    collectArticleId(this.articleId).then(res => {
-                        console.log(res);
-                        if (res.data.code == 0) {
-                            Toast.success("收藏成功");
-                            this.$emit("collect")
-                        }
+                    // this.$router.push("/my/login");
+                    Dialog.confirm({
+                        title: '您不登陆没办法收藏哦',
+                        message: '单击确认可以去到登录页面哦',
+                        confirmButtonText:'这就去登录',
+                        confirmButtonColor:'#424874',
+                        cancelButtonText:'就不去就不去'
                     })
+                        .then(() => {
+                            // on confirm
+                            this.$router.push("/my/login");
+                        })
+                        .catch(() => {
+                            // on cancel
+                        });
+                    // Toast.fail('请先登录，再进行操作');
                 }
-            }
+            },
+            // collect() {
+            //
+            //     if (this.isCollect) {
+            //         collectArticleId(this.articleId)
+            //             .then(res => {
+            //                 console.log(res);
+            //                 if (res.data.code == 0) {
+            //                     Toast.success("取消收藏");
+            //                     this.$emit("collect")
+            //                 }
+            //             })
+            //     } else {
+            //         collectArticleId(this.articleId).then(res => {
+            //             console.log(res);
+            //             if (res.data.code == 0) {
+            //                 Toast.success("收藏成功");
+            //                 this.$emit("collect")
+            //             }
+            //         })
+            //     }
+            // }
 
         },
     }
 </script>
 <style scoped lang="less">
-    .collent {
+    .collect {
         font-size: 30px;
         position: fixed;
         left: 70px;
